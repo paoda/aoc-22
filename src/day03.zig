1db14aca4b59fd81b440ee41d6c34c82d2b3ee53
Rekai Musuka <rekai@musuka.dev>
2022-12-03 01:41:26 -0400

    feat: complete day 3

--- src/day03.zig
+++ src/day03.zig
@@ -10,8 +10,105 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day03.txt");
 
+const example =
+    \\vJrwpWtwJgWrhcsFMMfFFhFp
+    \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+    \\PmmdzqPrVvPwwTWBwg
+    \\wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+    \\ttgJtRGJQctTZtZT
+    \\CrZsJsPPZsGzwwsLwLmpwMDw
+;
+
 pub fn main() !void {
-    
+    part1(data);
+    try part2(data);
+}
+
+fn part1(input: []const u8) void {
+    var rucksacks = split(u8, input, "\n");
+
+    var sum: u32 = 0;
+
+    while (rucksacks.next()) |rucksack| {
+        const left_compartment = rucksack[0 .. rucksack.len / 2];
+        const right_compartment = rucksack[rucksack.len / 2 .. rucksack.len];
+
+        print("Rucksack: {s}\n", .{rucksack});
+        print("left: {s}\n", .{left_compartment});
+        print("right: {s}\n", .{right_compartment});
+
+        blk: for (left_compartment) |left| {
+            for (right_compartment) |right| {
+                if (left == right) {
+                    // item is in both rucksacks, add to list
+
+                    print("Found: {s}\n", .{[_]u8{left}});
+
+                    // Convert to priorities;
+                    switch (left) {
+                        'a'...'z' => sum += left - 96,
+                        'A'...'Z' => sum += left - 38,
+                        else => unreachable,
+                    }
+
+                    break :blk;
+                }
+            }
+        }
+
+        print("Continuing...\n", .{});
+    }
+
+    print("Part 1: {}\n", .{sum});
+}
+
+fn part2(input: []const u8) !void {
+    var it = split(u8, input, "\n");
+
+    var sum: u32 = 0;
+
+    var rucksacks = std.ArrayList([]const u8).init(std.heap.page_allocator);
+    while (it.next()) |rucksack| try rucksacks.append(rucksack);
+    defer rucksacks.deinit();
+
+    var i: u32 = 0;
+    while (i < rucksacks.items.len) : (i += 3) {
+        if (i + 2 >= rucksacks.items.len) continue;
+
+        const left_rucksack = rucksacks.items[i];
+
+        const middle_rucksack = rucksacks.items[i + 1];
+        const right_rucksack = rucksacks.items[i + 2];
+
+        blk: for (left_rucksack) |left| {
+            for (middle_rucksack) |middle| {
+                for (right_rucksack) |right| {
+                    if (left == middle and middle == right) {
+                        // We've found the bage
+
+                        print("Found: {s}\n", .{[_]u8{left}});
+
+                        // Convert to priorities;
+                        switch (left) {
+                            'a'...'z' => sum += left - 96,
+                            'A'...'Z' => sum += left - 38,
+                            else => unreachable,
+                        }
+
+                        break :blk;
+                    }
+                }
+            }
+        }
+    }
+
+    print("Part 2: {}\n", .{sum});
+}
+
+const Context = struct {};
+
+fn lessThan(_: Context, left: u8, right: u8) bool {
+    return left < right;
 }
 
 // Useful stdlib functions
