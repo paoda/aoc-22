44e2e8048d2092da20520c4dffaaa595b729d58c
Rekai Musuka <rekai@musuka.dev>
2022-12-03 00:46:10 -0400

    feat: complete day 2

--- src/day02.zig
+++ src/day02.zig
@@ -10,8 +10,48 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+const example =
+    \\A Y
+    \\B X
+    \\C Z
+;
+
+const Action = enum(u32) { Rock = 1, Paper = 2, Scissors = 3 };
+const Result = enum(u32) { Loss = 0, Draw = 3, Win = 6 };
+
 pub fn main() !void {
-    
+    // part1(data);
+
+    var rounds = split(u8, data, "\n");
+    var part1_ans: u32 = 0;
+    var part2_ans: u32 = 0;
+
+    while (rounds.next()) |_round| {
+        const round = trim(u8, _round, &std.ascii.whitespace);
+        if (round.len == 0) continue; // some lines are blank
+
+        const opponent = round[0]; // Opponent Move
+        const player = round[2]; // Player Move
+
+        part1_ans += part1(player, opponent);
+        part2_ans += part2(player, opponent);
+    }
+
+    print("Part 1: {}\n", .{part1_ans});
+    print("Part 2: {}\n", .{part2_ans});
+}
+
+fn part1(player: u8, opponent: u8) u32 {
+    const lut: [3]Result = .{ .Win, .Loss, .Draw };
+    const diff = player - opponent;
+
+    return @enumToInt(lut[diff % 3]) + player - ('X' - 1);
+}
+
+fn part2(player: u8, opponent: u8) u32 {
+    const lut: [3]Action = .{ .Scissors, .Rock, .Paper };
+
+    return @enumToInt(lut[(player + opponent) % 3]) + 3 * (player - 'X');
 }
 
 // Useful stdlib functions
